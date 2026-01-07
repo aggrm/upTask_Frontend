5b63efef78a91c4e639bd9827b4cfec7eb72ec69
Alberto Goujon <alberto.goujon@gmail.com>
2026-01-07 19:10:42 +0100

    creacion de proyecto y primeros pasos del router

---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import AppLayout from './layouts/AppLayout'
+import DashboardView from './views/DashboardView'
+
+export default function router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<AppLayout />}>
+                    <Route path='/' element={<DashboardView />} index></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
