c6a0ce3a7ee34d0de226f8121bd73e9a05849277
Alberto Goujon <alberto.goujon@gmail.com>
2026-01-08 03:53:10 +0100

    devtools gestion de api

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import './index.css'
 import Router from './router'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Router/>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </StrictMode>,
 )
