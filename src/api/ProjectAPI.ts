fa746c73155b3267b75d3929017379402f07b65e
Alberto Goujon <alberto.goujon@gmail.com>
2026-01-08 03:32:09 +0100

    vista Actualizacion de datos proyecto

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,5 @@
 import api from "@/lib/axios";
-import { dashBoardProjectSchema, type ProjectFormData } from "../types";
+import { dashBoardProjectSchema, type Project, type ProjectFormData } from "../types";
 import { isAxiosError } from "axios";
 
 
@@ -22,6 +22,32 @@ export async function getAllProjects() {
         if(response.success){
             return response.data
         }
+    } catch (error) {
+        if(isAxiosError(error) && error.response){
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function getProjectById(id: Project['_id']) {
+    try {
+        const { data } = await api.get(`/projects/${id}`)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response){
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+type ProjectAPIType = {
+    formData: ProjectFormData,
+    projectId: Project['_id']
+}
+export async function updateProject({formData, projectId} : ProjectAPIType) {
+    try {
+        const { data } = await api.put<string>(`/projects/${projectId}`, formData)
+        return data
     } catch (error) {
         if(isAxiosError(error) && error.response){
             throw new Error(error.response.data.error)
--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import AppLayout from '@/layouts/AppLayout'
 import DashboardView from '@/views/DashboardView'
 import CreateProjectView from './views/projects/CreateProjectView'
+import EditProjectView from './views/projects/EditProjectView'
 
 export default function router() {
     return (
@@ -10,8 +11,10 @@ export default function router() {
                 <Route element={<AppLayout />}>
                     <Route path='/' element={<DashboardView />} index></Route>
                     <Route path='/projects/create' element={<CreateProjectView />} index></Route>
+                    <Route path='/projects/:projectId/edit' element={<EditProjectView />} index></Route>
                 </Route>
             </Routes>
         </BrowserRouter>
     )
 }
+
