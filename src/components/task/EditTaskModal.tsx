a3a409b99247df55f8a25690ad52dfaf2cd19443
Alberto Goujon <alberto.goujon@gmail.com>
2026-01-22 00:50:54 +0100

    crud finalizado de las tareas

--- src/components/task/EditTaskModal.tsx
+++ src/components/task/EditTaskModal.tsx
@@ -1,10 +1,51 @@
 import { Fragment } from 'react';
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
-import { replace, useNavigate } from 'react-router-dom';
+import { replace, useNavigate, useParams } from 'react-router-dom';
+import type { TaskFormData, Task } from '@/types/index';
+import { useForm } from 'react-hook-form';
+import TaskForm from './TaskForm';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { updateTask } from '@/api/TaskAPI';
+import { toast } from 'react-toastify';
 
-export default function EditTaskModal() {
+type EditTaskModalProps = {
+    data: Task
+    taskId: string
+}
+
+export default function EditTaskModal({data, taskId}: EditTaskModalProps) {
 
     const navigate = useNavigate()
+
+    /** Obtener projectId */
+    const params = useParams()
+    const projectId = params.projectId!
+
+    const {register, handleSubmit, reset, formState: {errors}} = useForm<TaskFormData>({defaultValues: {
+        name: data.name,
+        description: data.description
+    }})
+
+    const queryClient = useQueryClient()
+
+    const {mutate} = useMutation({
+        mutationFn: updateTask,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({queryKey: ['project', projectId]})
+            queryClient.invalidateQueries({queryKey: ["task", taskId]})
+            toast.success(data)
+            reset()
+            navigate(location.pathname, {replace:true})
+        }
+    })
+
+    const handleEditTask = (formData: TaskFormData) => {
+        const data = { projectId, taskId, formData }
+        mutate(data)
+    }
 
     return (
         <Transition appear show={true} as={Fragment}>
@@ -46,11 +87,14 @@ export default function EditTaskModal() {
 
                                 <form
                                     className="mt-10 space-y-3"
+                                    onSubmit={handleSubmit(handleEditTask)}
                                     noValidate
                                 >
-                    
+                                    <TaskForm 
+                                        register={register} 
+                                        errors={errors}
+                                    />
 
-                    
                                     <input
                                         type="submit"
                                         className=" bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3  text-white font-black  text-xl cursor-pointer"
--- src/components/task/TaskCard.tsx
+++ src/components/task/TaskCard.tsx
@@ -1,8 +1,11 @@
-import type { Task } from "@/types/index"
-import { Menu, MenuButton, MenuItem, MenuItems, Transition } from "@headlessui/react"
-import { EllipsisVerticalIcon } from "@heroicons/react/20/solid"
 import { Fragment } from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
+import { Menu, MenuButton, MenuItem, MenuItems, Transition } from "@headlessui/react"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { EllipsisVerticalIcon } from "@heroicons/react/20/solid"
+import { toast } from "react-toastify"
+import type { Task } from "@/types/index"
+import { deleteTaskById } from "@/api/TaskAPI"
 
 
 type TaskCardProps = {
@@ -12,6 +15,23 @@ type TaskCardProps = {
 export default function TaskCard({task}: TaskCardProps) {
 
     const navigate = useNavigate()
+    /** Obtener projectId */
+    const params = useParams()
+    const projectId = params.projectId!
+
+    const queryClient = useQueryClient()
+
+    const { mutate } = useMutation({
+        mutationFn: deleteTaskById,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+            queryClient.invalidateQueries({queryKey: ['project', projectId]})
+        }
+     })
+    
   return (
     <li className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
         <div className="min-w-0 flex flex-col gap-y-4">
@@ -48,7 +68,11 @@ export default function TaskCard({task}: TaskCardProps) {
                         </MenuItem>
 
                         <MenuItem>
-                            <button type='button' className='block px-3 py-1 text-sm leading-6 text-red-500'>
+                            <button 
+                                type='button' 
+                                className='block px-3 py-1 text-sm leading-6 text-red-500'
+                                onClick={() => mutate({projectId, taskId : task._id})}
+                            >
                                 Eliminar Tarea
                             </button>
                         </MenuItem>
