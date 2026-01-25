e73883b494e4079eb7d847fb68730e43c60f0328
Alberto Goujon <alberto.goujon@gmail.com>
2026-01-25 21:56:59 +0100

    añadida la creacion de cuentas

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import AppLayout from '@/layouts/AppLayout'
 import DashboardView from '@/views/DashboardView'
+import LoginView from '@/views/auth/loginView'
 import CreateProjectView from './views/projects/CreateProjectView'
 import EditProjectView from './views/projects/EditProjectView'
 import ProjectDetailsView from './views/projects/ProjectDetailsView'
+import AuthLayout from './layouts/AuthLayout'
+import RegisterView from './views/auth/RegisterView'
 
 export default function router() {
     return (
@@ -14,6 +17,11 @@ export default function router() {
                     <Route path='/projects/create' element={<CreateProjectView />} index></Route>
                     <Route path='/projects/:projectId' element={<ProjectDetailsView />} index></Route>
                     <Route path='/projects/:projectId/edit' element={<EditProjectView />} index></Route>
+                </Route>
+
+                <Route element={<AuthLayout />}>
+                    <Route path='/auth/login' element={<LoginView />} index></Route>
+                    <Route path='/auth/register' element={<RegisterView />} index></Route>
                 </Route>
             </Routes>
         </BrowserRouter>
