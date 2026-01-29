9ab3388d04388f7b0c2381ffa4c98211c096f355
aggrm <alberto.goujon@gmail.com>
2026-01-29 01:10:01 +0100

    Proteccion de app con bearer token en API RESTful y comprobacion de usuarios logados

--- src/types/index.ts
+++ src/types/index.ts
@@ -19,6 +19,15 @@ export type NewPasswordForm = Pick<Auth, 'password' | 'password_confirmation'>
 
 export type ConfirmToken = Pick<Auth, 'token'>
 
+/** Users */
+export const userSchema = authSchema.pick({ 
+    name: true,
+    email: true
+}).extend({
+    _id: z.string()
+})
+
+export type User = z.infer<typeof userSchema>
 /** Task */
 export const taskStatusSchema = z.enum (["pending", "onHold","inProgress", "underReview", "completed"])
 export type TaskStatus = z.infer<typeof taskStatusSchema>
