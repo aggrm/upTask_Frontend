ea38884cc658c5f7ba0ec36a29100142cb567a2f
aggrm <alberto.goujon@gmail.com>
2026-02-07 17:34:02 +0100

    Preparacion de componentres para gestionar el perfil del usuario.

--- src/router.tsx
+++ src/router.tsx
@@ -12,6 +12,9 @@ import RequestNewCodeView from './views/auth/RequestNewCodeView'
 import NewPasswordView from './views/auth/NewPasswordView'
 import ForgotPasswordView from './views/auth/ForgotPasswordView'
 import ProjectTeamView from './views/projects/ProjectTeamView'
+import ProfileView from './views/profile/ProfileView'
+import ChangePasswordView from './views/profile/ChangePasswordView'
+import ProfileLayout from './layouts/ProfileLayout'
 
 
 export default function router() {
@@ -20,19 +23,23 @@ export default function router() {
             <Routes>
                 <Route element={<AppLayout />}>
                     <Route path='/' element={<DashboardView />} index></Route>
-                    <Route path='/projects/create' element={<CreateProjectView />} index></Route>
-                    <Route path='/projects/:projectId' element={<ProjectDetailsView />} index></Route>
-                    <Route path='/projects/:projectId/edit' element={<EditProjectView />} index></Route>
-                    <Route path='/projects/:projectId/team' element={<ProjectTeamView />} index></Route>
+                    <Route path='/projects/create' element={<CreateProjectView />} />
+                    <Route path='/projects/:projectId' element={<ProjectDetailsView />} />
+                    <Route path='/projects/:projectId/edit' element={<EditProjectView />} />
+                    <Route path='/projects/:projectId/team' element={<ProjectTeamView />} />
+                    <Route element={<ProfileLayout />}>
+                        <Route path='/profile/' element={<ProfileView />} />
+                        <Route path='/profile/password' element={<ChangePasswordView/>} />
+                    </Route>
                 </Route>
 
                 <Route element={<AuthLayout />}>
-                    <Route path='/auth/login' element={<LoginView />} index></Route>
-                    <Route path='/auth/register' element={<RegisterView />} index></Route>
-                    <Route path='/auth/confirm-account' element={<ConfirmAccountView />} index></Route>
-                    <Route path='/auth/request-code' element={<RequestNewCodeView/>} index></Route>
-                    <Route path='/auth/forgot-password' element={<ForgotPasswordView/>} index></Route>
-                    <Route path='/auth/new-password' element={<NewPasswordView/>} index></Route>
+                    <Route path='/auth/login' element={<LoginView />} />
+                    <Route path='/auth/register' element={<RegisterView />} />
+                    <Route path='/auth/confirm-account' element={<ConfirmAccountView />} />
+                    <Route path='/auth/request-code' element={<RequestNewCodeView/>} />
+                    <Route path='/auth/forgot-password' element={<ForgotPasswordView/>} />
+                    <Route path='/auth/new-password' element={<NewPasswordView/>} />
                 </Route>
             </Routes>
         </BrowserRouter>
